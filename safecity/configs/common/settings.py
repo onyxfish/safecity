0a0adc1fbe712192e52461ea5521ccecb00ea02f
bouvard <staringmonkey@gmail.com>
2010-03-27 19:05:20 -0500

    Imported newsapps template w/ safecitychicago.org config.

---
+++ safecity/configs/common/settings.py
@@ -0,0 +1,118 @@
+import logging
+import os
+
+import django
+
+# Base paths
+DJANGO_ROOT = os.path.dirname(os.path.realpath(django.__file__))
+SITE_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+# Debugging
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('Christopher Groskopf', 'safecitychicago@gmail.com'),
+)
+
+MANAGERS = ADMINS
+
+# Database
+# Note: DATABASE_USER and DATABASE_PASSWORD are defined in the staging and
+# production settings.py files. For local use, either define them in
+# local_settings.py or ignore to use your local user.
+DATABASE_ENGINE = 'postgresql_psycopg2'
+DATABASE_HOST = 'localhost'
+DATABASE_PORT = '5432'
+DATABASE_NAME = 'safecity'
+
+# Local time
+TIME_ZONE = 'America/Chicago'
+
+# Local language
+LANGUAGE_CODE = 'en-gb'
+
+# Site framework
+SITE_ID = 1
+
+# Internationalization
+USE_I18N = False
+
+# Absolute path to the directory that holds media.
+MEDIA_ROOT = os.path.join(SITE_ROOT, 'assets')
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = ''
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/media/'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'y3-4hsx8jhby=go$q5*3&p@1-r+02xrc!sj&mw%#c__s=9(xzr'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+    'django.template.loaders.eggs.load_template_source',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.gzip.GZipMiddleware',
+    'django.middleware.cache.UpdateCacheMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
+)
+
+ROOT_URLCONF = 'safecity.configs.common.urls'
+
+TEMPLATE_DIRS = (
+    os.path.join(SITE_ROOT, 'templates')
+)
+
+INSTALLED_APPS = (
+    'django.contrib.contenttypes',
+    'django.contrib.sites',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'django.contrib.humanize',
+    'django.contrib.gis',
+    'django.contrib.sitemaps',
+)
+
+# Predefined domain
+MY_SITE_DOMAIN = 'localhost:8000'
+
+# Email
+# run "python -m smtpd -n -c DebuggingServer localhost:1025" to see outgoing
+# messages dumped to the terminal
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
+DEFAULT_FROM_EMAIL = 'safecitychicago@gmail.com'
+
+# Caching
+CACHE_MIDDLEWARE_KEY_PREFIX='safecity'
+CACHE_MIDDLEWARE_SECONDS=90 * 60 # 90 minutes
+CACHE_BACKEND="dummy:///"
+
+# Analytics
+GOOGLE_ANALYTICS_KEY = "UA-15515833-1"
+
+# Logging
+logging.basicConfig(
+    level=logging.DEBUG,
+)
+
+# Allow for local (per-user) override
+try:
+    from local_settings import *
+except ImportError:
+    pass
