1d396a361bceba245ae5c2cca76d3be78907e3b2
bouvard <staringmonkey@gmail.com>
2010-04-10 14:28:13 -0500

    Added missing urls.py

---
+++ safecity/apps/zeep/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import *
+
+from safecity.apps.zeep import views
+
+urlpatterns = patterns('',
+    url("^incoming/",
+        views.incoming,
+        name="zeep_incoming"),
+)
