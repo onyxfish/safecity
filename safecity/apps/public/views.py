65276fdea1f862b1106aae7e3dba6a5372187fa5
bouvard <staringmonkey@gmail.com>
2010-04-18 11:28:17 -0500

    Fleshed out infrastructure for public pages.

--- safecity/apps/public/views.py
+++ safecity/apps/public/views.py
@@ -1,7 +1,9 @@
 import logging
 log = logging.getLogger("safecity.public.views")
 
+from django.conf import settings
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 from safecity.apps.danger.models import Report
 
@@ -10,4 +12,10 @@ def index(request):
     Render the index page that displays anonymized data for public
     consumption.
     """
-    return HttpResponse(content='TODO')
+    context = {
+        'settings': settings,
+    }
+    
+    return render_to_response(
+        'public/index.html',
+        context)
