3e9101ef97719d2fb740f99b42ad60024c82ee75
bouvard <staringmonkey@gmail.com>
2010-04-24 11:10:25 -0500

    Fixed public code faux paux.

--- safecity/apps/tropo/tropolib.py
+++ safecity/apps/tropo/tropolib.py
@@ -10,7 +10,6 @@ from django.http import HttpResponse
 from safecity.lib.messages import *
 
 TROPO_URL = 'http://api.tropo.com/1.0/sessions'
-TROPO_TOKEN = '610c578baff94548b4c19d3389494bea03905dc92b60f6d84c7aa9e26fa10688b242f1f151a0cf1f1d993ad2'
 
 # Exceptions
 
@@ -33,7 +32,7 @@ class TropoOutgoingMessage(OutgoingMessage):
         
         params = {
             'action': 'create',
-            'token': TROPO_TOKEN,
+            'token': settings.TROPO_TOKEN,
             'recipients': ','.join(self.recipients),
             'text': self.text,
         }
